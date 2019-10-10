8b538903ec32e16f58fa97e1c1576f9687481cf7
Hiroki Katagiri <h.katagiri.0358@gmail.com>
2019-10-10 16:57:08 +0900

    Add support of STS authentication

--- lib/sqs_checker/sqs_helper.rb
+++ lib/sqs_checker/sqs_helper.rb
@@ -23,11 +23,33 @@ module SqsChecker
       @queue_name = queue_name
     end
 
+    def aws_credentials
+      @aws_credentials ||= @credentials['session_token'] ? sts_credentials : normal_credentials
+    end
+
+    def normal_credentials
+      ::Aws::Credentials.new(
+        access_key_id: @credentials.fetch('access_key_id'),
+        secret_access_key: @credentials.fetch('secret_access_key'),
+      )
+    end
+
+    def sts_credentials
+      ::Aws::AssumeRoleCredentials.new(
+        client: ::Aws::STS::Client.new(
+          access_key_id: @credentials.fetch('access_key_id'),
+          secret_access_key: @credentials.fetch('secret_access_key'),
+          session_token: @credentials.fetch('session_token'),
+        ),
+        role_arn: @credentials.fetch('role_arn'),
+        role_session_name: 'sqs_checker',
+      )
+    end
+
     # @return [Aws::SQS::Client]
     def sqs_client
       @sqs_client ||= ::Aws::SQS::Client.new(
-        access_key_id: @credentials.fetch('access_key_id'),
-        secret_access_key: @credentials.fetch('secret_access_key'),
+        credentials: aws_credentials,
         region: @credentials.fetch('region'),
       )
     end
